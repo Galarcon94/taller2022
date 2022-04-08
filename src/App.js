2bfaff397ba7e4cf6d939528eace3001d12b388c
Gabriel <87220227+Galarcon94@users.noreply.github.com>
2022-04-08 13:56:38 -0500

    fin XD

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
+import React, { useState } from "react";
 import "./styles.css";
 import Componente from "./apps/Francisco/main";
 import AppKaren from "./apps/Karen/main";
-import GabrielApp from "./apps/Gabriel/main";
+//import GabrielApp from "./apps/Gabriel/main";
 import Componente2 from "./apps/Pedro/main";
-import Erik from "./apps/Erick/main";
+import Erick from "./apps/Erick/main";
 import AppVictor from "./apps/Victor/main";
+
 export default function App() {
+  const [_sel, set_sel] = useState("");
+
   return (
-    <div className="App">
+    <div>
       <h1>Taller desarrollo web colaborativo con React 2022</h1>
-      <h2>Todos nuestros participantes :)</h2>
-      <GabrielApp />
-      <Componente />
-      <AppKaren />
-      <Componente2 />
-      <Erik />
-      <AppVictor />
+      <h2>Nuestros colaboradores :)</h2>
+      <button
+        onClick={() => {
+          set_sel("Francisco");
+        }}
+      >
+        Francisco
+      </button>
+      <button
+        onClick={() => {
+          set_sel("Karen");
+        }}
+      >
+        Karen
+      </button>
+      <button
+        onClick={() => {
+          set_sel("Pedro");
+        }}
+      >
+        Pedro
+      </button>
+      <button
+        onClick={() => {
+          set_sel("Erick");
+        }}
+      >
+        Erick
+      </button>
+      <button
+        onClick={() => {
+          set_sel("Victor");
+        }}
+      >
+        Victor
+      </button>
+      <br />
+      Muchas gracias n.n/
+      <hr />
+      <div>
+        {_sel === "Francisco" && <Componente />}
+        {_sel === "Karen" && <AppKaren />}
+        {_sel === "Pedro" && <Componente2 />}
+        {_sel === "Erick" && <Erick />}
+        {_sel === "Victor" && <AppVictor />}
+      </div>
     </div>
   );
 }
---
+++ src/apps/Karen/main.jsx
@@ -0,0 +1,189 @@
+import estilo from "./css/estilo.module.css";
+import encabezado from "./img/encabezado.png";
+import circulo from "./img/round.jpg";
+import pie from "./img/pie.png";
+export default function AppKaren() {
+  return (
+    <div className="App">
+      <h1 className={estilo.titulo1_h1}>Dr. Jacobo Grinberg-Zylberbaum</h1>
+      <nav className={estilo.nav_est}>
+        <ul>
+          <h3>Entre la ciencia y el chamanismo mexicano</h3>
+        </ul>
+      </nav>
+      <div align="center">
+        <img src={encabezado} alt="encabezado" />
+      </div>
+      <div className={estilo.contenido}>
+        <div className={estilo.informacion}>
+          <div className={estilo.secciones}>
+            <div>
+              <h2>
+                <big>¿Quién es Jacobo Grinberg?</big>
+              </h2>
+              <wbr />
+              <p>
+                <big>
+                  <br />
+                  Jacobo Grinberg-Zylberbaum (Ciudad de México, 12 de diciembre
+                  de 1946-1994), conocido como Jacobo Grinberg, fue un
+                  neurofisiólogo y psicólogo mexicano. Se dedicó a estudiar el
+                  chamanismo mexicano, la conciencia, disciplinas orientales,
+                  meditación, astrología y telepatía. Escribió 54 libros acerca
+                  de estos temas. Desapareció el 8 de diciembre de 1994, en
+                  circunstancias desconocidas.
+                  <br />
+                  <br />
+                  Jacobo Grinberg-Zylberbaum nació en la Ciudad de México en
+                  1946. Decidió estudiar por primera vez la mente humana cuando
+                  tenía doce años, después de que su madre muriera de un
+                  accidente cerebrovascular. Estudió psicología en la
+                  Universidad Nacional Autónoma de México. En 1970, viajó a
+                  Nueva York a estudiar psicofisiología en el Brain Research
+                  Institute (Instituto de Investigación Cerebral). Obtuvo un
+                  doctorado enfocado en los efectos electrofisiológicos de los
+                  estímulos geométricos en el cerebro humano.
+                  <br />
+                  <br />A su regreso a México, fundó un laboratorio de
+                  psicofisiología en la Universidad Anáhuac. Posteriormente, a
+                  finales de la década de 1970, instaló otro laboratorio similar
+                  en la Universidad Nacional Autónoma de México (UNAM). En 1987,
+                  fundó el Instituto Nacional Para el Estudio de la Conciencia
+                  (INPEC), y financiado por la UNAM y el Consejo Nacional de
+                  Ciencia y Tecnología (Conacyt). Jacobo Grinberg publicó varios
+                  de sus libros a través del INPEC. Escribió más de 50 libros
+                  acerca de la actividad cerebral, la brujería, el chamanismo,
+                  la telepatía y la meditación.​
+                  <br />
+                  <br />
+                  Jacobo Grinberg puso su reputación como científico en peligro
+                  cuando intentó utilizar el método científico en estudios sobre
+                  chamanismo, al intentar combinar ambos en su trabajo
+                  profesional con el objetivo de entender el "mundo mágico".
+                  Intentó realizar un cambio en la manera en la que se entiende
+                  la relación entre la ciencia y la conciencia. Su trabajo
+                  exhibió las bases científicas para la telepatía, aunque fue
+                  rechazado por sus colegas científicos.​
+                  <br />
+                  <br />
+                  Desde el 8 de diciembre de 1994, Jacobo Grinberg se encuentra
+                  desaparecido. El 12 de diciembre, su familia preparó una
+                  fiesta en su honor para celebrar su cumpleaños número 48, pero
+                  él no se presentó. Era común que él realizara viajes de último
+                  minuto o que simplemente no contestara su teléfono por días,
+                  por lo que su desaparición al principio no pareció extrañar a
+                  su familia.​
+                  <br />
+                  <br />
+                  La teoría sintérgica de Jacobo Grinberg propone que existe un
+                  continuo espacio de energía y que el humano común sólo puede
+                  percibir una parte de este. El resultado de este proceso es lo
+                  que todo el mundo entiende como "realidad". Esta teoría
+                  intenta responder a la pregunta de la creación de la
+                  experiencia. El libro "El cerebro consciente" se tradujo a
+                  siete idiomas.​
+                  <br />
+                  <br />
+                  La teoría sintérgica nació de su trabajo con Bárbara Guerrero,
+                  una psíquica y sanadora mexicana conocida como Pachita.
+                  Pachita realizaba cirugía psíquica. Grinberg escribió un
+                  libro, Pachita, analizando las bases científicas de sus
+                  experiencias con ella. También está enlistada en su libro
+                  Chamanes de México. Grinberg estaba intrigado por la
+                  reputación de Pachita, por lo que siguió sus procedimientos
+                  durante alrededor de un año. Llegó a la conclusión de que la
+                  habilidad de Pachita para sanar era el resultado de dos
+                  diferentes realidades: tanto a la presencia de un campo
+                  neuronal que rodea nuestros cerebros como la presencia de una
+                  "red espacio-temporal". A pesar de que la mayoría de
+                  científicos consideran que la habilidad de Pachita para sanar
+                  es un fraude, las investigaciones de Grinberg acerca de la
+                  transferencia de potencial en el cerebro se publicaron en
+                  Physics Essays, una revista científica arbitrada.​
+                  <br />
+                </big>
+              </p>
+            </div>
+          </div>
+          <div align="center">
+            <img src={pie} alt="pie" />
+          </div>
+        </div>
+
+        <div className={estilo.equipo}>
+          <p>
+            <b>Algunos de sus libros más famosos</b>
+          </p>
+          <wbr />
+          <p>
+            <i>La Teoría Sintérgica (1991)</i>
+          </p>
+          <wbr />
+          <p>
+            <i>
+              Serie: Los Chamanes de México con un total de 7 volumenes
+              publicados entre 1987 y 1990
+            </i>
+          </p>
+          <wbr />
+          <p>
+            <i>El cerebro consciente (1979)</i>
+          </p>
+          <wbr />
+          <p>
+            <i>Psicofisiología del Aprendizaje (1976)</i>
+          </p>
+          <wbr />
+          <p>
+            <i>La Creación de la Experiencia (1990)</i>
+          </p>
+          <wbr />
+          <p>
+            <i>El Espacio y la Conciencia (1981)</i>
+          </p>
+          <wbr />
+          <p>
+            <i>Fluir en El sin yo (1991)</i>
+          </p>
+          <wbr />
+          <p>
+            <i>La Construcción de la Realidad (1975)</i>
+          </p>
+          <wbr />
+          <p>
+            <i>Las Creaciones de la Existencia (1976)</i>
+          </p>
+          <wbr />
+          <p>
+            <i>Los Fundamentos de la Experiencia (1978)</i>
+          </p>
+          <wbr />
+          <p>
+            <i>El YO como idea (1994)</i>
+          </p>
+          <wbr />
+          <p>
+            <i></i>
+          </p>
+          <wbr />
+          <div>
+            <img className={estilo.round_img} src={circulo} alt="circulo" />
+          </div>
+        </div>
+      </div>
+
+      <footer className={estilo.footer}>
+        <nav>
+          <center>
+            <big>
+              <h4>Ana Karen Díaz Gómez</h4>
+              <h3>Taller de desarrollo web Colaborativo con React</h3>
+              <h4>Ing. Gabriel Alarcón Carranza</h4>
+            </big>
+          </center>
+        </nav>
+        <wbr />
+      </footer>
+    </div>
+  );
+}
