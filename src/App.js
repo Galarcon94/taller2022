947e04f0e8b42c918ff20cd28d3324d2c19c7f5c
Ana <87220348+Kgoiaz@users.noreply.github.com>
2022-04-08 10:33:06 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Componente from "./apps/Francisco/main";
+import AppKaren from "./apps/Karen/main";
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <Componente />
+      <AppKaren />
     </div>
   );
 }
