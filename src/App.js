ff133278d895f9db1a12c9c260aec0ad6065c5b8
Epictrolloleante <dark1473@gmail.com>
2022-04-08 10:28:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Componente from "./apps/Francisco/main";
+import Componente2 from "./apps/plantilla/Pedro/main";
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <Componente />
+      <Componente2 />
     </div>
   );
 }
